673123aeeddc7344620bf5091b9e311c0758e97a
Kozlov <dp200890kev@gmail.com>
2020-05-04 13:58:40 +0200

    add github actions, eslint, badge

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,8 @@
 import readlineSync from 'readline-sync';
 
 const play = () => {
-    console.log('Welcome to the Brain Games!');
-    const nameUser = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${nameUser}!`);
-}
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameUser}!`);
+};
 export default play;
