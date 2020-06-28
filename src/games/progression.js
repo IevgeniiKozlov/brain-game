bb57020e07ba87c91d7eacc54ce03126409e70af
Kozlov Evgeniy <dp200890kev@gmail.com>
2020-06-28 13:26:30 +0200

    fix v4 finaly corrections according to the mentor

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,7 +1,6 @@
 import getRandomInt from '../random-integer.js';
 import letPlay from '../cli.js';
 
-
 const description = 'What number is missing in the progression?';
 
 const lengthProgression = 10;
@@ -13,9 +12,9 @@ const getProgression = (start, step, iter) => {
 };
 
 const generateGameData = () => {
-  const num = getRandomInt(1, 100);
+  const startElement = getRandomInt(1, 100);
   const step = getRandomInt(1, 10);
-  const progression = getProgression(num, step, 1).flat(Infinity);
+  const progression = getProgression(startElement, step, 1).flat(Infinity);
   const index = getRandomInt(0, progression.length - 1);
   const answer = progression[index].toString();
   progression[index] = '..';
