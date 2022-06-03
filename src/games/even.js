8c910b6d55597e88f1a47c0a505298c72efea654
Kozlov Evgeniy <dp200890kev@gmail.com>
2022-06-03 15:40:53 +0300

    update file

--- src/games/even.js
+++ src/games/even.js
@@ -6,8 +6,8 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no"';
 const isEven = (num) => (num % 2 === 0);
 
 const getEven = () => {
-  const num = getRandomInt(1, 100);
-  const answer = isEven(num) ? 'yes' : 'no';
+  const question = getRandomInt(1, 100);
+  const answer = isEven(question) ? 'yes' : 'no';
   return [String(question), answer];
 };
 
